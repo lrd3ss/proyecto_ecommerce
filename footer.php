2cfaab0796e68dba94f9e5106e9ca7c2e324ba81
Lurdes Medina <lurdes.medinadelatorre@gmail.com>
2023-05-29 18:44:48 -0400

    subida inicial de archivos
    subida inicial de archivos para sitio de ecommerce

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package proyecto_ecommerce
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'proyecto_ecommerce' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'proyecto_ecommerce' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'proyecto_ecommerce' ), 'proyecto_ecommerce', '<a href="http://proyecto.ecommerce.cl">Lurdes Medina</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
